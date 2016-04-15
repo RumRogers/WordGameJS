50bcf091ba570c5bae28e4864d78839740dafd83
andrea@biobeats.com <andreaserreli1984@gmail.com>
2016-04-15 19:46:04 +0200

    Implemented loadDictionary function
    The loadDictionary(relativePath) function makes it easy to possibly change the path of the wordlist.txt file without problems: it is enough to change its parameter to the new path

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 var MYAPP = MYAPP || {};
 
-$(document).ready(function() {
-        $.ajax({
-            url : "res/wordlist.txt",
-            dataType: "text",
-            success : function (data) {
-                MYAPP.dictionary = data.split('\n');
-            }
-        });
+MYAPP.loadDictionary = function(relativePath)
+{
+    $.ajax({
+        url : relativePath,
+        dataType: "text",
+        success : function (data)
+        {
+            MYAPP.dictionary = data.split('\n');
+            MYAPP.wordGame = new WordGame();
+            MYAPP.wordGame.getBaseString();
+        },
+        error : function()
+        {
+            throw "Unable to load " + relativePath + " ... Aborting.";
+        }
+    });
+};
 
-    MYAPP.wordGame = new WordGame();
-    MYAPP.wordGame.getBaseString();
+$(document).ready(function()
+{
+    MYAPP.loadDictionary('res/wordlist.txt');
 });
